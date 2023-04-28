b3087280bf2ad07cb0911e93092d8d4d508e0070
mauribarbona <fabianbarbona23@gmail.com>
2023-04-28 11:57:29 -0300

    primer cambio

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import CartWidget from "./CartWidget/CartWidget";
+
+
+const NavBar = () => {
+    return (
+        <nav>
+           <h1>E-commerce</h1>
+           <div>
+            <button>Celulares</button>
+            <button>Tablets</button>
+            <button>Notebooks</button>
+           </div>
+
+           <CartWidget/>
+            
+        </nav>
+    )
+}
+
+export default NavBar;
